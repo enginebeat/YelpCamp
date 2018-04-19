0b47ee01b932768d784f841cf9c58d533d66e0a0
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-04-19 15:28:44 +0100

    adding new campsite and some improvements

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ Each Camp will have
 *Image
 Other atributes will be added
 
-to start with this will be done in an array.
+to start with this will be done in an array.animation-timing-function: ease-in;
+
+# Create New Campgrounds
+
+- Setup new campgrounds Post Route
+
+#Style the campgrounds page
+
+- add a better header/title
+- make campgrounds display in a grid
--- v1/app.js
+++ v1/app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+bodyParser = require ('body-parser');
+
+var campgrounds = [
+    {name: 'Salmon Creek', image:'https://farm3.staticflickr.com/2031/2270475589_3858deb67e.jpg'},
+    {name: 'Granite Hill', image:'https://farm4.staticflickr.com/3231/2436513348_f617b82a9e.jpg'},
+    {name: "'Mountain Goat's Rest", image:'https://farm8.staticflickr.com/7399/9211397472_e6d0a7c6fd.jpg'}
+]
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('view engine', 'ejs');
 
 /* Routes
@@ -12,15 +22,25 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/campgrounds', (req, res)=>{
-    var campgrounds = [
-        {name: 'Salmon Creek', image:'https://pixabay.com/get/eb37b9082df3003ed1584d05fb1d4e97e07ee3d21cac104497f3c57aa6e8bcbf_340.jpg'},
-        {name: 'Granite Hill', image:'https://farm4.staticflickr.com/3231/2436513348_f617b82a9e.jpg'},
-        {name: "'Mountain Goat's Rest", image:'https://farm8.staticflickr.com/7399/9211397472_e6d0a7c6fd.jpg'}
-    ]
-
     res.render('campgrounds', {campgrounds: campgrounds});
 });
 
+app.post('/campgrounds', (req, res)=>{
+    //res.send('you hit the post route');
+    //get data from form
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    //add data to array
+    campgrounds.push(newCampground); 
+    // redirect to /campground
+    res.redirect('/campgrounds');
+
+});
+
+app.get('/campgrounds/new', (req, res)=>{
+    res.render('new.ejs');
+});
 
 app.listen(4000, ()=>{
     console.log('The YelpCamp Server Has Started');
